0c89edcd4dcc96fceb5a105d4bb5f0c80ac01388
Mariusz Macheta <mmacheta@future-processing.com>
2025-12-02 20:33:53 +0100

    Day02

---
+++ AdventOfCode2020/Day02/Program.cs
@@ -0,0 +1,32 @@
+﻿var passwords = File.ReadAllLines("input.txt")
+	.Select(Parse)
+	.ToList();
+
+var result1 = passwords.Where(p => p.IsValid1()).Count();
+var result2 = passwords.Where(p => p.IsValid2()).Count();
+
+Console.WriteLine("1: " +  result1);
+Console.WriteLine("2: " +  result2);
+
+PasswordLine Parse(string x)
+{
+	var parts = x.Split(" ");
+	var rangeParts = parts[0].Split("-");
+	return new PasswordLine(int.Parse(rangeParts[0]), int.Parse(rangeParts[1]), parts[1].First(), parts[2]);
+}
+
+public record PasswordLine(int policyLow, int policyHigh, char letter, string password)
+{
+	public bool IsValid1()
+	{
+		var count = password.Where(c => c == letter).Count();
+		return count >= policyLow && count <= policyHigh;
+	}
+
+	public bool IsValid2()
+	{
+		var n1 = password[policyLow - 1];
+		var n2 = password[policyHigh - 1];
+		return  (n1 == letter ||  n2 == letter) && n1 != n2;
+	}
+}
