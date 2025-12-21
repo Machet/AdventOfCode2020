d97f24fc119b6aa477479fce9ddb937a487581cd
Mariusz Macheta <mmacheta@future-processing.com>
2025-12-21 19:50:28 +0100

    Day06

---
+++ AdventOfCode2020/Day06/Program.cs
@@ -0,0 +1,29 @@
+﻿using Utils;
+
+var groups = File.ReadAllLines("input.txt")
+	.SplitByPredicate(string.IsNullOrEmpty)
+	.ToList();
+
+var uniqueAnswers = groups.Select(GetUniqueAnswerCount).ToList();
+Console.WriteLine("1: " + uniqueAnswers.Sum());
+
+var commonAnswers = groups.Select(GetCommonAnswerCount).ToList();
+Console.WriteLine("2: " + commonAnswers.Sum());
+
+int GetUniqueAnswerCount(List<string> answers)
+{
+	return answers.SelectMany(l => l).ToHashSet().Count();
+}
+
+int GetCommonAnswerCount(List<string> answers)
+{
+	var groups = answers.Select(x => x.ToHashSet());
+	var set = groups.First();
+
+	foreach (var answer in answers)
+	{
+		set = set.Intersect(answer).ToHashSet();
+	}
+
+	return set.Count;
+}
