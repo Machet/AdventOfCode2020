54e5305bb386f50207806fa7a483d65e7590555d
Mariusz Macheta <mmacheta@future-processing.com>
2026-01-03 22:24:32 +0100

    Day15

---
+++ AdventOfCode2020/Day15/Program.cs
@@ -0,0 +1,28 @@
+﻿var numbers = File.ReadAllText("input.txt")
+	.Split(",")
+	.Select(int.Parse)
+	.ToList();
+
+var turnMemory = numbers.Select((n, i) => (n, i)).ToDictionary(p => p.n, p => p.i + 1);
+var hasBeenSpokenMemory = new HashSet<int>(numbers.Take(numbers.Count - 1));
+
+var turn = numbers.Count;
+var lastNumberSpoken = numbers.Last();
+
+while (turn < 30000000)
+{
+	turn++;
+	var newSpoken = !hasBeenSpokenMemory.Add(lastNumberSpoken)
+		? turn - 1 - turnMemory[lastNumberSpoken]
+		: 0;
+
+	turnMemory[lastNumberSpoken] = turn - 1;
+	lastNumberSpoken = newSpoken;
+
+	if (turn == 2020)
+	{
+		Console.WriteLine("1: " + lastNumberSpoken);
+	}
+}
+
+Console.WriteLine("2: " + lastNumberSpoken);
